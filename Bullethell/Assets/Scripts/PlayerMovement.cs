e24487cafaf0d44d6523b3dc4464cbca9b8b4755
jpaesch00 <56660070+jpaesch00@users.noreply.github.com>
2019-10-16 17:58:32 -0700

    Player Character
    Added movable player w/ hitbox
    Renamed SampleScene
    Added script to move player

---
+++ Bullethell/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float moveX;
+    private float moveY;
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector2(moveX, moveY) * speed;
+    }
+}
